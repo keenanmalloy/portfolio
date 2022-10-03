68d04525027e3073a3326fcefb8947f15e08651d
keenanmalloy <malloyrkeenan@gmail.com>
2022-10-03 15:50:30 -0700

    portfolio

---
+++ components/Hero.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React from "react";
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
+import { PageInfo } from "../typings";
+import { urlFor } from "../sanity";
+
+type Props = {
+  pageInfo: PageInfo;
+};
+
+export default function Hero({ pageInfo }: Props) {
+  const [text, count] = useTypewriter({
+    words: [`Hi, My Name Is ${pageInfo?.name}.`],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+
+      <Image
+        src={urlFor(pageInfo?.heroImage).url()}
+        className="relative rounded-full  mx-auto object-cover"
+        alt="image"
+        height={128}
+        width={128}
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          {pageInfo?.role}
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
