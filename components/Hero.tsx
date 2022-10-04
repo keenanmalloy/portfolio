950b2bc2c0ac9eed07d759621581d342e8cfc022
keenanmalloy <malloyrkeenan@gmail.com>
2022-10-04 13:02:22 -0700

    new typewriter

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,19 @@
 import Image from "next/image";
 import React from "react";
-import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Link from "next/link";
 import { PageInfo } from "../typings";
 import { urlFor } from "../sanity";
+import Typewriter from "typewriter-effect";
 
 type Props = {
   pageInfo: PageInfo;
 };
 
 export default function Hero({ pageInfo }: Props) {
-  const [text, count] = useTypewriter({
-    words: [`Hi, My Name Is ${pageInfo?.name}.`],
-    loop: true,
-    delaySpeed: 2000,
-  });
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-
       <Image
         src={urlFor(pageInfo?.heroImage).url()}
         className="relative rounded-full  mx-auto object-cover"
@@ -27,14 +21,25 @@ export default function Hero({ pageInfo }: Props) {
         height={128}
         width={128}
       />
+      <Typewriter
+        options={{
+          strings: [
+            `Hi, my name is ${pageInfo?.name}.`,
+            "Junior Web Developer.",
+          ],
+          autoStart: true,
+          loop: true,
+          cursor: "|",
+          cursorClassName: "text-[#F7AB0A] text-5xl lg:text-6xl",
+          wrapperClassName: "text-5xl lg:text-6xl font-semibold scroll-px-10 ",
+        }}
+      />
+
       <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           {pageInfo?.role}
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
-          <span className="mr-3">{text}</span>
-          <Cursor cursorColor="#F7AB0A" />
-        </h1>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10"></h1>
         <div className="pt-5">
           <Link href="#about">
             <button className="heroButton">About</button>
