50969700507dd3aa61310a925fa32dbc2354ca9f
keenanmalloy <95385630+keenanmalloy@users.noreply.github.com>
2022-10-12 10:00:08 -0700

    contact form test

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -12,7 +12,7 @@ export default function ContactMe() {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
-      <div className="flex flex-col space-y-5 sm:space-y-10">
+      <div className="flex flex-col space-y-5 pb-10">
         <h4 className="text-4xl font-semibold text-center">
           <span className="underline decoration-[#F7AB0A]/50">Lets Talk.</span>
         </h4>
