f88d0724049b592015085721476414c49b8aa073
keenanmalloy <95385630+keenanmalloy@users.noreply.github.com>
2022-10-12 10:35:02 -0700

    mobile test 6

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -7,7 +7,7 @@ export default function Skill({ skillIcon }) {
       {
         <img
           alt="skill icon"
-          className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+          className="rounded-full border border-gray-500 object-cover w-16 h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
           src={skillIcon}
         />
       }
